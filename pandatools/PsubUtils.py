059f37a937f6003a69b7edd6c2d64a63cd3008de
tmaeno <tmaeno@d4488797-78bc-619e-cc48-e4164af92aee>
2010-11-24 20:15:25 +0000

    0.3.19

--- pandatools/PsubUtils.py
+++ pandatools/PsubUtils.py
@@ -1569,7 +1569,7 @@ def writeJobDefID(jobID):
 
 
 # calculate the number of subjobs
-def calculateNumSplit(nFilesPerJob,nGBPerJob,nEventsPerJob,
+def calculateNumSplit(nFilesPerJob,nGBPerJob,nEventsPerJob,nEventsPerFile,
                       maxTotalSize,dbrDsSize,safetySize,useTagParentLookup,
                       inputFileList,inputFileMap,tagFileList,parentLfnToTagMap):
     # count total size for inputs
@@ -1664,8 +1664,11 @@ def calculateNumSplit(nFilesPerJob,nGBPerJob,nEventsPerJob,
         tmpSubTotal = 0
         tmpSubNumFiles = 0
         for fileName in inputFileList:
-            vals = inputFileMap[fileName]
-            tmpSize = long(vals['fsize'])
+            if inputFileMap.has_key(fileName):
+                vals = inputFileMap[fileName]
+                tmpSize = long(vals['fsize'])
+            else:
+                tmpSize = 0
             tmpSubNumFiles += 1    
             singleLargeFile = False
             if tmpSubTotal+tmpSize > nGBPerJob-dbrDsSize-safetySize \
