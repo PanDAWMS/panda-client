7b811776debde102c7ce986388fae5f6cc15f41d
tmaeno <tmaeno@d4488797-78bc-619e-cc48-e4164af92aee>
2012-05-09 07:21:04 +0000

    0.3.97

--- pandatools/AthenaUtils.py
+++ pandatools/AthenaUtils.py
@@ -574,10 +574,10 @@ def convStrToFullPathJobOs(tmpStr):
 def copyAthenaStuff(currentDir):
     baseName = os.environ['PANDA_SYS'] + "/etc/panda/share"
     for tmpFile in athenaStuff:
-        com = 'cp %s/%s %s' % (baseName,tmpFile,currentDir)
+        com = 'cp -p %s/%s %s' % (baseName,tmpFile,currentDir)
         commands.getoutput(com)
     for fullJobO,localName in fullPathJobOs.iteritems():
-        com = 'cp %s %s/%s' % (fullJobO,currentDir,localName)
+        com = 'cp -p %s %s/%s' % (fullJobO,currentDir,localName)
         commands.getoutput(com)
 
 
@@ -1854,6 +1854,9 @@ def getCmtConfig(athenaVer=None,cacheVer=None,nightVer=None,cmtConfig=None):
             maVer = int(match.group(1))
             miVer = int(match.group(2))
             reVer = int(match.group(3))
+            # use x86_64-slc5-gcc43-opt for 17.5.0 or higher
+            if maVer > 17 or (maVer == 17 and miVer > 5) or (maVer == 17 and miVer == 5 and reVer >= 0):
+                return 'x86_64-slc5-gcc43-opt'
             # use i686-slc5-gcc43-opt for 15.6.3 or higher
             if maVer > 15 or (maVer == 15 and miVer > 6) or (maVer == 15 and miVer == 6 and reVer >= 3):
                 return 'i686-slc5-gcc43-opt'
