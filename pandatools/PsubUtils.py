0121a64dbce2d93992007c23f658712093c64fc3
tmaeno <tmaeno@d4488797-78bc-619e-cc48-e4164af92aee>
2013-03-02 17:48:49 +0000

    * added a debug option to pathena * added --useMana to prun * changed tgz/rpm repository due to changes of CERN twiki policy

--- pandatools/PsubUtils.py
+++ pandatools/PsubUtils.py
@@ -748,7 +748,7 @@ def updatePackage(verbose=False):
         packageName = 'panda-client-%s.tar.gz' % latestVer
     else:
         packageName = 'panda-client-%s-1.noarch.rpm' % latestVer
-    com = 'wget --no-check-certificate --timeout 120 https://twiki.cern.ch/twiki/pub/Atlas/PandaTools/%s' \
+    com = 'wget --no-check-certificate --timeout 120 https://atlpan.web.cern.ch/atlpan/panda-client/%s' \
           % packageName
     status = os.system(com)
     status %= 255    
