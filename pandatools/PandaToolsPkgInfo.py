e0fda97adc154c9b3ed86a2f682ef61e4122420a
tmaeno <tmaeno@bnl.gov>
2021-03-23 08:10:44 +0100

    1.4.59

--- pandatools/PandaToolsPkgInfo.py
+++ pandatools/PandaToolsPkgInfo.py
@@ -1,1 +1,1 @@
-release_version = "1.4.59"
+release_version = "1.4.60"
--- pandatools/idds_api.py
+++ pandatools/idds_api.py
@@ -4,8 +4,10 @@ from . import Client
 # API call class
 class IddsApi(object):
 
-    def __init__(self, name, dumper, verbose):
+    def __init__(self, name, dumper, verbose, idds_host):
         self.name = name
+        if idds_host is not None:
+            self.name += '+{}'.format(idds_host)
         self.dumper = dumper
         self.verbose = verbose
 
@@ -19,11 +21,12 @@ class IddsApiInteface(object):
         self.dumper = None
 
     def __getattr__(self, item):
-        return IddsApi(item, self.dumper, self.verbose)
+        return IddsApi(item, self.dumper, self.verbose, self.idds_host)
 
-    def setup(self, dumper, verbose):
+    def setup(self, dumper, verbose, idds_host):
         self.dumper = dumper
         self.verbose = verbose
+        self.idds_host = idds_host
 
 
 # entry for API
@@ -31,14 +34,15 @@ api = IddsApiInteface()
 del IddsApiInteface
 
 
-def get_api(dumper=None, verbose=False):
+def get_api(dumper=None, verbose=False, idds_host=None):
     """Get an API object to access iDDS through PanDA
 
        args:
            dumper: function object to json-serialize data
            verbose: True to see verbose messages
+           idds_host: iDDS hostname
        return:
            an API object
     """
-    api.setup(dumper, verbose)
+    api.setup(dumper, verbose, idds_host)
     return api
