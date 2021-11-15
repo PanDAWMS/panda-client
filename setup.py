f570cdf34b034e4b71b4bc731501f42ddaa385e0
Matthew Feickert <matthew.feickert@cern.ch>
2021-11-15 15:31:35 -0600

    docs: Add Python version support to PyPI metadata

--- setup.py
+++ setup.py
@@ -155,6 +155,18 @@ setup(
     author_email='atlas-adc-panda@cern.ch',
     url='https://panda-wms.readthedocs.io/en/latest/',
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 
     # optional pip dependencies
     extras_require={
