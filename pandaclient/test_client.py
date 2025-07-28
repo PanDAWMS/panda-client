d01ec96449ef9d12f2b6b3eb1c59a267e0763de8
Fernando Barreiro <fh.barreiro@gmail.com>
2025-07-28 18:11:32 +0200

    test_client.py to accept a list of panda IDs from command line

--- pandaclient/test_client.py
+++ pandaclient/test_client.py
@@ -1,7 +1,13 @@
+import argparse
 from pandaclient.Client import getJobStatus, getJobStatus_new
 
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Process panda IDs.")
+parser.add_argument("panda_ids", nargs="*", type=int, help="List of panda IDs")
+args = parser.parse_args()
+panda_ids = args.panda_ids
+
 print("=============================================================")
-panda_ids = [6744530186]
 old_ret = getJobStatus(ids=panda_ids, verbose=True)
 print ("------------------------------------------------------------")
 new_ret = getJobStatus_new(ids=panda_ids, verbose=True)
