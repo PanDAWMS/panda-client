c9cb73419ddfe78696c54220dc8053449a1dd2eb
Fernando Barreiro <fh.barreiro@gmail.com>
2025-07-29 11:09:30 +0200

    Added killJobs_new to test cases

--- pandaclient/test_client.py
+++ pandaclient/test_client.py
@@ -1,5 +1,5 @@
 import argparse
-from pandaclient.Client import getJobStatus, getJobStatus_new
+from pandaclient.Client import getJobStatus, getJobStatus_new, killJobs, killJobs_new
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Process panda IDs.")
@@ -18,12 +18,9 @@ print("new function returned: {}".format(new_ret))
 new_ret[1][0].print_attributes()
 
 print("=============================================================")
-panda_ids = []
-old_ret = getJobStatus(ids=panda_ids, verbose=True)
+old_ret = killJobs(ids=panda_ids, verbose=True)
 print ("------------------------------------------------------------")
-new_ret = getJobStatus_new(ids=panda_ids, verbose=True)
+new_ret = killJobs_new(ids=panda_ids, verbose=True)
 
 print("old function returned {}".format(old_ret))
-old_ret[1][0].print_attributes()
 print("new function returned: {}".format(new_ret))
-new_ret[1][0].print_attributes()
