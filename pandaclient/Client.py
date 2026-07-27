5353afb8b9c486a7128f2d17d8f12d8fdc843ca0
Fernando Barreiro <fh.barreiro@gmail.com>
2026-07-27 11:16:28 +0200

    Catching exceptions during _build_ssl_context

--- pandaclient/Client.py
+++ pandaclient/Client.py
@@ -495,6 +495,7 @@ class _HttpClient:
         url = self.randomize_ip(url)
         if data:
             url = f"{url}?{urlencode(data, doseq=repeating_keys)}"
+
         try:
             verify = self._build_ssl_context(use_https)
         except Exception as e:
@@ -537,7 +538,12 @@ class _HttpClient:
         """
         use_https = is_https(url)
         url = self.randomize_ip(url)
-        verify = self._build_ssl_context(use_https)
+
+        try:
+            verify = self._build_ssl_context(use_https)
+        except Exception as e:
+            return 1, str(f"Exception setting up the SSL environment with credentials: {e}")
+
         headers = self._auth_headers()
         if compress_body or json_out:
             headers["Content-Type"] = "application/json"
@@ -582,7 +588,12 @@ class _HttpClient:
         """
         use_https = is_https(url)
         url = self.randomize_ip(url)
-        verify = self._build_ssl_context(use_https)
+
+        try:
+            verify = self._build_ssl_context(use_https)
+        except Exception as e:
+            return 1, str(f"Exception setting up the SSL environment with credentials: {e}")
+
         headers = self._auth_headers()
         if json_out:
             headers["Accept"] = "application/json"
